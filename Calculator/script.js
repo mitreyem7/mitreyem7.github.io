af1a229ac38541fecbd925ba1e28a6437c0faa8a
mitreyem <tfoxmeyer@gmail.com>
2021-06-28 16:24:13 -0500

    clock and calc

---
+++ Calculator/script.js
@@ -0,0 +1,280 @@
+const endsWithOperator = /[-\+\*\/]$/;
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formula: "",
+      done: false,
+      currentValue: "0" };
+
+    this.allClear = this.allClear.bind(this);
+    this.evalFormula = this.evalFormula.bind(this);
+    this.handleNumber = this.handleNumber.bind(this);
+    this.handleEqual = this.handleEqual.bind(this);
+    this.handleOperator = this.handleOperator.bind(this);
+    this.backSpace = this.backSpace.bind(this);
+
+  }
+
+  allClear() {
+    this.setState({
+      formula: "",
+      done: false,
+      currentValue: "0" });
+
+  }
+
+  evalFormula() {
+    let currentFormula = this.state.formula;
+    currentFormula = currentFormula.replace(/-{2}/g, "+");
+    currentFormula = currentFormula.replace(/([-\+\*\/])([\+\*\/])/g, '$2');
+    this.setState({
+      formula: currentFormula });
+
+
+    return currentFormula == "" ?
+    0 :
+    Math.round(1000000000000 * eval(currentFormula)) / 1000000000000;
+  }
+
+  handleNumber(e) {
+    if (this.state.done) {
+      this.setState({
+        formula: "",
+        currentValue: e.target.value.toString(),
+        done: false });
+
+    } else {
+      if (endsWithOperator.test(this.state.currentValue)) {
+        this.setState({
+          formula: this.state.formula + this.state.currentValue,
+          currentValue: e.target.value.toString() });
+
+      } else {
+        if (!this.state.currentValue.includes(".") ||
+        e.target.value.toString() != ".") {
+          if (this.state.currentValue == "0") {
+            this.setState({
+              currentValue: e.target.value.toString() });
+
+          } else {
+            this.setState({
+              currentValue: this.state.currentValue +
+              e.target.value.toString() });
+
+          }
+        }
+      }
+    }
+
+  }
+  handleOperator(e) {
+    let newOperator = e.target.value.toString();
+    let newFormula = this.state.currentValue;
+    if (this.state.done) {
+      this.setState({
+        formula: newFormula,
+        currentValue: e.target.value.toString(),
+        done: false });
+
+    } else {
+
+      if (parseFloat(newFormula) > 0) {
+        this.setState({
+          formula: this.state.formula + newFormula,
+          done: false,
+          currentValue: newOperator });
+
+      } else {
+
+
+        if (endsWithOperator.test(newFormula)) {
+          if (newOperator == "-") {
+            this.setState({
+              formula: this.state.formula + newFormula,
+              done: false });
+
+          }
+
+          this.setState({
+            currentValue: newOperator,
+            done: false });
+
+
+        }
+      }
+    }
+  }
+
+  handleEqual() {
+    let lastValue = this.state.currentValue;
+    if (endsWithOperator.test(lastValue)) {
+      lastValue = "";
+    }
+    this.setState({
+      formula: this.state.formula + lastValue },
+    () => {
+      this.setState({
+        currentValue: this.evalFormula(),
+        done: true });
+    });
+  }
+
+  backSpace() {
+    if (this.state.currentValue.length > 0) {
+      this.setState({
+        currentValue: this.state.currentValue.substring(
+        0, this.state.currentValue.length - 1),
+        done: false });
+
+
+    }
+  }
+
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("div", { id: "app" }, /*#__PURE__*/
+      React.createElement("div", { id: "output" }, /*#__PURE__*/
+      React.createElement("div", { id: "result" },
+      this.state.formula), /*#__PURE__*/
+
+      React.createElement("div", { id: "display" },
+      this.state.currentValue)), /*#__PURE__*/
+
+
+      React.createElement("div", { id: "buttons" }, /*#__PURE__*/
+      React.createElement("button", {
+        id: "clear",
+        onClick: this.allClear }, "AC"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "back",
+        onClick: this.backSpace }, "<--"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "divide",
+        onClick: this.handleOperator,
+        value: "/" }, "/"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "multiply",
+        onClick: this.handleOperator,
+        value: "*" }, "*"), /*#__PURE__*/
+
+
+
+
+      React.createElement("button", {
+        id: "seven",
+        onClick: this.handleNumber,
+        value: "7" }, "7"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "eight",
+        onClick: this.handleNumber,
+        value: "8" }, "8"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "nine",
+        onClick: this.handleNumber,
+        value: "9" }, "9"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "subtract",
+        onClick: this.handleOperator,
+        value: "-" }, "-"), /*#__PURE__*/
+
+
+
+
+      React.createElement("button", {
+        id: "four",
+        onClick: this.handleNumber,
+        value: "4" }, "4"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "five",
+        onClick: this.handleNumber,
+        value: "5" }, "5"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "six",
+        onClick: this.handleNumber,
+        value: "6" }, "6"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "add",
+        onClick: this.handleOperator,
+        value: "+" }, "+"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "one",
+        onClick: this.handleNumber,
+        value: "1" }, "1"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "two",
+        onClick: this.handleNumber,
+        value: "2" }, "2"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "three",
+        onClick: this.handleNumber,
+        value: "3" }, "3"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "equals",
+        onClick: this.handleEqual }, "="), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "zero",
+        onClick: this.handleNumber,
+        value: "0" }, "0"), /*#__PURE__*/
+
+
+
+      React.createElement("button", {
+        id: "decimal",
+        onClick: this.handleNumber,
+        value: "." }, "."))));
+
+
+
+
+
+
+  }}
+
+
+
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Calculator, null), document.getElementById("root"));
---
+++ Clock/script.js
@@ -0,0 +1,204 @@
+const beep = document.getElementById("beep");
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25,
+      timerType: "Session",
+      timer: 1500 };
+
+    this.decBreakLength = this.decBreakLength.bind(this);
+    this.incBreakLength = this.incBreakLength.bind(this);
+    this.decSessLength = this.decSessLength.bind(this);
+    this.incSessLength = this.incSessLength.bind(this);
+    this.resetClick = this.resetClick.bind(this);
+    this.decrementTimer = this.decrementTimer.bind(this);
+    this.changeStatus = this.changeStatus.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+
+  }
+
+  decBreakLength() {
+    if (this.state.breakLength > 1) {
+      this.setState({
+        timer: this.state.timerType == "Break" ?
+        (this.state.breakLength - 1) * 60 :
+        this.state.timer,
+        breakLength: this.state.breakLength - 1 });
+
+    }
+    document.getElementById("start_stop").className = "fas fa-play fa-2x";
+    clearInterval(this.timerInterval);
+  }
+  incBreakLength() {
+    if (this.state.breakLength < 60) {
+      this.setState({
+        timer: this.state.timerType == "Break" ?
+        (this.state.breakLength + 1) * 60 :
+        this.state.timer,
+        breakLength: this.state.breakLength + 1 });
+
+    }
+    document.getElementById("start_stop").className = "fas fa-play fa-2x";
+    clearInterval(this.timerInterval);
+  }
+  decSessLength() {
+    if (this.state.sessionLength > 1) {
+      this.setState({
+        timer: this.state.timerType == "Session" ?
+        (this.state.sessionLength - 1) * 60 :
+        this.state.timer,
+        sessionLength: this.state.sessionLength - 1 });
+
+    }
+    document.getElementById("start_stop").className = "fas fa-play fa-2x";
+    clearInterval(this.timerInterval);
+  }
+  incSessLength() {
+    if (this.state.sessionLength < 60) {
+      this.setState({
+        timer: this.state.timerType == "Session" ?
+        (this.state.sessionLength + 1) * 60 :
+        this.state.timer,
+        sessionLength: this.state.sessionLength + 1 });
+
+    }
+    document.getElementById("start_stop").className = "fas fa-play fa-2x";
+    clearInterval(this.timerInterval);
+  }
+
+  decrementTimer() {
+    if (this.state.timer == 0) {
+      beep.play();
+      const wasType = this.state.timerType;
+      switch (wasType) {
+        case "Session":
+          this.setState({
+            timerType: "Break",
+            timer: this.state.breakLength * 60 });
+
+          break;
+        case "Break":
+          this.setState({
+            timerType: "Session",
+            timer: this.state.sessionLength * 60 });
+
+          break;
+        default:
+          break;}
+
+    } else {
+      this.setState({ timer: this.state.timer - 1 });
+    }
+  }
+
+  startTimer() {
+    this.timerInterval = setInterval(() => this.decrementTimer(), 1000);
+
+  }
+
+  resetClick() {
+    document.getElementById("start_stop").className = "fas fa-play fa-2x";
+    clearInterval(this.timerInterval);
+    this.setState({
+      breakLength: 5,
+      sessionLength: 25,
+      timerType: "Session",
+      timer: 1500 });
+
+    beep.pause();
+    beep.currentTime = 0;
+  }
+
+  changeStatus() {
+    let statusButton = document.getElementById("start_stop");
+    let currentButton = statusButton.className;
+    switch (currentButton) {
+      case "fas fa-play fa-2x":
+        statusButton.className = "fas fa-pause fa-2x";
+        this.startTimer();
+        break;
+      case "fas fa-pause fa-2x":
+        statusButton.className = "fas fa-play fa-2x";
+        clearInterval(this.timerInterval);
+        break;
+      default:
+        break;}
+
+
+  }
+
+
+  render() {
+    let minutes = Math.floor(this.state.timer / 60);
+    let seconds = this.state.timer - minutes * 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+
+    return /*#__PURE__*/(
+      React.createElement("div", { id: "app" }, /*#__PURE__*/
+      React.createElement("h1", null, "25 + 5 Clock"), /*#__PURE__*/
+      React.createElement("div", { id: "break-label", className: "control" }, /*#__PURE__*/
+      React.createElement("p", null, "Break Length", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/
+      React.createElement("i", { class: "fas fa-minus",
+        id: "break-decrement",
+        onClick: this.decBreakLength }), /*#__PURE__*/
+
+      React.createElement("span", { id: "break-length" },
+      this.state.breakLength), /*#__PURE__*/
+
+      React.createElement("i", { class: "fas fa-plus",
+        id: "break-increment",
+        onClick: this.incBreakLength }))), /*#__PURE__*/
+
+
+
+
+      React.createElement("div", { id: "session-label", className: "control" }, /*#__PURE__*/
+      React.createElement("p", null, "Session Length", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/
+      React.createElement("i", { class: "fas fa-minus",
+        id: "session-decrement",
+        onClick: this.decSessLength }), /*#__PURE__*/
+
+      React.createElement("span", { id: "session-length" },
+      this.state.sessionLength), /*#__PURE__*/
+
+      React.createElement("i", { class: "fas fa-plus",
+        id: "session-increment",
+        onClick: this.incSessLength }))), /*#__PURE__*/
+
+
+
+
+
+
+      React.createElement("div", { id: "timer-wrapper" }, /*#__PURE__*/
+      React.createElement("div", { id: "timer-label" },
+      this.state.timerType), /*#__PURE__*/
+
+      React.createElement("div", { id: "time-left" },
+      minutes + ":" + seconds), /*#__PURE__*/
+
+      React.createElement("div", { id: "timer-controls" }, /*#__PURE__*/
+      React.createElement("i", { class: "fas fa-play fa-2x",
+        id: "start_stop",
+        onClick: this.changeStatus }), /*#__PURE__*/
+
+      React.createElement("i", { class: "fas fa-redo-alt fa-2x",
+        id: "reset",
+        onClick: this.resetClick })))));
+
+
+
+
+
+
+  }}
+
+
+
+
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Clock, null), document.getElementById('root'));
